851527c2102bb3f8964db395ac22905839daffc0
LaPlace24 <67650717+LaPlace24@users.noreply.github.com>
2023-07-31 19:40:27 -0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,22 @@
+# Definición de variables de distintos tipos primitivos
+cadena_string = "Hola, soy una cadena"
+num_entero = 42
+num_flotante = 3.14
+tipo_booleano = True
+# Concatenación de las variables en una cadena
+resultado_concatenacion = cadena_string + " " + str(num_entero) + " " + str(num_flotante) + " " + str(tipo_booleano)
+# Fórmula para la suma de los primeros n números pares: n * (n + 1)
+n = 10  # Puedes cambiar este valor para probar con diferentes n
+
+suma_numeros_pares = n * (n + 1)
+
+# Impresión de los resultados
+print("Variable cadena:", cadena_string)
+print("Variable entero:", num_entero)
+print("Variable flotante:", num_flotante)
+print("Variable booleano:", tipo_booleano)
+
+print("Resultado de la concatenación:", resultado_concatenacion)
+
+print("Suma de los primeros", n, "números pares:", suma_numeros_pares)
+
